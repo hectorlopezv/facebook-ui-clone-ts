07d657c96f4581e3d2a9236c17d28264baaf0a5f
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-12-29 12:55:09 -0500

    finishish setup project, using BEM convention, Redux, MaterialUi for this build

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,4 +1,23 @@
-body {
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle<any>`
+    * 
+    {
+    margin: 0;
+    padding: 0;
+    }
+    
+    *, *::after, *::before 
+    {
+        box-sizing: inherit;
+    }
+
+    html 
+    {
+        box-sizing: border-box;
+    }
+
+    body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -11,3 +30,6 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`;
+
+export default GlobalStyle;
---
+++ src/lib/firebase.prod.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVFCFyGpymuhJ2JUbxfXyJ-cr6nDL54DM",
+    authDomain: "facebook-ui-clone.firebaseapp.com",
+    projectId: "facebook-ui-clone",
+    storageBucket: "facebook-ui-clone.appspot.com",
+    messagingSenderId: "175634521835",
+    appId: "1:175634521835:web:25ff124aa3b76e7ddbe8ae"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
