f7dc2a6c4c4f6ac27c5da35d8554eb4347018ee9
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-12-30 16:36:47 -0500

    post component done

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel/StoryReel';
 import Message from './Message/Message';
+import Post from './Post/Post';
 export interface FeedProps {
     
 }
@@ -11,11 +12,34 @@ const Feed: React.FC<FeedProps> = () => {
         <div className="feed">
             <StoryReel />
             <Message />
+            <Post
+                profilePic={""}
+                message={"hector message"}
+                timestamp={"this is a timeStamp"}
+                username={"this is a username"}
+                image={""}
+            />
+            <Post
+                profilePic={""}
+                message={"hector message"}
+                timestamp={"this is a timeStamp"}
+                username={"this is a username"}
+                image={""}
+            />
+            {/*Widgets */}
             
-            {/*MessageSender */}
-            {/*Posts */}
         </div>
     );
 }
  
+// {posts.map(post => (
+//     <Post
+//         key={post.data.id}
+//         profilePic={post.data.profilePic}
+//         message={post.data.message}
+//         timestamp={post.data.timestamp}
+//         username={post.data.username}
+//         image={post.data.image}
+//     />
+//     ))}
 export default Feed;
