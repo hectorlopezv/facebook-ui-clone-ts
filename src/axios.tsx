4de151c9579b1880a0f89ad68f50c0956dde31da
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2021-01-02 00:44:29 -0500

    updating rest api linl

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://facebook-mongodob-backedn.herokuapp.com/'
 });
 
 export default instance;
